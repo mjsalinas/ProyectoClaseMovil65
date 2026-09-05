df01a23a4f76f9eca052951cf000d10756d7ac46
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-09-04 20:18:37 -0600

    definicion de reducer en el store

--- MyRoutineApp/src/store/index.ts
+++ MyRoutineApp/src/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userProfileReducer from "./slices/userProfileSlice";
 
 export const store = configureStore ({
-    reducer: {}, 
+    reducer: {
+        userProfile: userProfileReducer
+    }, 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
