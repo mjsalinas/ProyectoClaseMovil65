80d7c518d8b490d133c13cd2b31930c4891b07c9
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-09-07 20:22:46 -0600

    agregando logica a reducers

--- MyRoutineApp/src/store/slices/userProfileSlice.ts
+++ MyRoutineApp/src/store/slices/userProfileSlice.ts
@@ -21,20 +21,34 @@ const userProfileSlice = createSlice({
     name: "userProfile",
     initialState: initialUserProfile,
     reducers: {
-        updateProfile: (state, action: PayloadAction<UserProfile>)=>{
-
+        updateProfile: (state, action: PayloadAction<UserProfile>) => {
+            Object.assign(state, action.payload);
         },
-        addMedicalCondition: (state, action: PayloadAction<string>)=>{
-            
+        //las modificaciones a las propiedades tipo arreglo, se hacen directamente al objeto [medicalConditions] en el estado
+        addMedicalCondition: (state, action: PayloadAction<string>) => {
+            //aplicar validaciones de formato y de tipo de dato para agregar un elemento nuevo a un arreglo
+            const trimmed = action.payload.trim();
+            if (!trimmed || state.medicalConditions.includes(trimmed)) return;
+            state.medicalConditions.push(trimmed);
         },
-        removeMedicalCondition: (state, action: PayloadAction<string>)=>{
-            
+        removeMedicalCondition: (state, action: PayloadAction<string>) => {
+            //eliminar un elemento de un arreglo segun coincidencia de contenido (valor a eliminar viene del payload)
+            state.medicalConditions = state.medicalConditions.filter(
+                (mc) => mc !== action.payload
+            );
         },
-        addTreatment: (state, action: PayloadAction<string>)=>{
-
+        //las modificaciones a las propiedades tipo arreglo, se hacen directamente al objeto [dermatologicalTreatments] en el estado
+        addTreatment: (state, action: PayloadAction<string>) => {
+            //aplicar validaciones de formato y de tipo de dato para agregar un elemento nuevo a un arreglo
+            const trimmed = action.payload.trim();
+            if (!trimmed || state.medicalConditions.includes(trimmed)) return;
+            state.dermatologicalTreatments.push(trimmed);
         },
-        removeTreatment: (state, action: PayloadAction<string>)=>{
-
+        removeTreatment: (state, action: PayloadAction<string>) => {
+            //eliminar un elemento de un arreglo segun coincidencia de contenido (valor a eliminar viene del payload)
+            state.dermatologicalTreatments = state.dermatologicalTreatments.filter(
+                (dt) => dt !== action.payload
+            )
         },
     },
 });
