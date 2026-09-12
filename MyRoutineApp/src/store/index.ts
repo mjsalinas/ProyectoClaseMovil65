6e634c96b08f1241a683791ab40d21aa4ea46bd5
KennethC01 <kennethcaballerol14@gmail.com>
2026-09-11 20:02:47 -0600

    store/index.ts - se registro skincareReducer bajo la clave skincare

--- MyRoutineApp/src/store/index.ts
+++ MyRoutineApp/src/store/index.ts
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userProfileReducer from "./slices/userProfileSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import userProfileReducer from './slices/userProfileSlice';
+import skincareReducer from './slices/skincareSlice'; 
 
-export const store = configureStore ({
-    reducer: {
-        userProfile: userProfileReducer
-    }, 
+export const store = configureStore({
+  reducer: {
+    userProfile: userProfileReducer,
+    skincare: skincareReducer,  
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
