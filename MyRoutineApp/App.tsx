3fcb0052f5aad960a4606cd9364ab9f23600a03a
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-09-11 19:11:34 -0600

    integrando product detail en el stack y recibiendo parametro por ruta

--- MyRoutineApp/App.tsx
+++ MyRoutineApp/App.tsx
@@ -6,6 +6,7 @@ import { navigationRef } from "./src/navigation/NavigationService";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { SkincareProvider } from "./src/contexts/SkincareContext";
 import { store } from "./src/store";
 
 function AppNavigation() {
@@ -27,7 +28,9 @@ export default function App() {
       <ThemeProvider>
         <LanguageProvider>
           <AuthProvider>
-            <AppNavigation />
+            <SkincareProvider>
+              <AppNavigation />
+            </SkincareProvider>
           </AuthProvider>
         </LanguageProvider>
       </ThemeProvider>
--- MyRoutineApp/src/navigation/StackNavigator.tsx
+++ MyRoutineApp/src/navigation/StackNavigator.tsx
@@ -2,12 +2,14 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../screens/Login";
 import Register from "../screens/Register";
 import TabNavigator from "./TabsNavigator";
+import ProductDetail from "../screens/ProductDetail";
 import { useTheme } from "../contexts/ThemeContext";
 
 export type RootStackParamList = {
   LoginScreen: undefined;
   RegisterScreen: undefined;
   UserTabs: undefined;
+  ProductDetail: { productId: string };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -24,12 +26,25 @@ export default function StackNavigator() {
         contentStyle: { backgroundColor: colors.background },
       }}
     >
-      <Stack.Screen name="LoginScreen" component={Login} />
-      <Stack.Screen name="RegisterScreen" component={Register} />
+      <Stack.Screen
+        name="LoginScreen"
+        component={Login}
+        options={{ title: "Skincare Tracker" }}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={Register}
+        options={{ title: "Registro" }}
+      />
       <Stack.Screen
         name="UserTabs"
         component={TabNavigator}
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ProductDetail"
+        component={ProductDetail}
+        options={{ title: "Detalle del producto" }}
       />
     </Stack.Navigator>
   );
